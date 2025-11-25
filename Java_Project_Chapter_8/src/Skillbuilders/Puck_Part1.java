82de7c15786e022f5ad2db32a279ce8db2986511
38247004 <38247004@L815WD220024.edu.cbe.ab.ca>
2025-11-25 10:22:24 -0700

    Nov 25 2025

---
+++ Java_Project_Chapter_8/src/Skillbuilders/Puck_Part1.java
@@ -0,0 +1,12 @@
+package Skillbuilders;
+
+public class Puck_Part1 extends Disk {
+	//Define local variables
+	private double weight;
+	private boolean standard;
+	private boolean youth;
+	
+	//Constructs a new puck
+	
+
+}
