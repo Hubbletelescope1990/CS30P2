89daecd7bdc3eeb7ba627264b37f9c1c673ac0a2
38247004 <38247004@L815WD220024.edu.cbe.ab.ca>
2025-12-05 10:28:00 -0700

    Dec 5 2025

--- Java_Project_Chapter_8/src/Mastery/University_staff_roster.java
+++ Java_Project_Chapter_8/src/Mastery/University_staff_roster.java
@@ -13,7 +13,7 @@ public class University_staff_roster {
 		Staff employee2 = new Staff("Elizabeth", "Buisness", 165000, "Department head");
 		Staff employee3 = new Staff("Alex", "Maintenence", 80000, "Janitor");
 		
-		int choice;
+		int choice = 20;
 		String action = null;
 		
 		do {
@@ -24,8 +24,19 @@ public class University_staff_roster {
 		System.out.println("0: shutdown application");
 		
 		choice = input.nextInt();
+		Staff employee = null;
 		
-		if(choice <= 3) {
+		if (choice == 1) {
+			employee = employee1;
+		}
+		if (choice == 2) {
+			employee = employee2;
+		}
+		if (choice == 3) {
+			employee = employee3;
+		}		
+		
+		if(choice <= 3 && choice > 0) {
 			System.out.println("What would you like to do with employee " + choice + "?");
 			System.out.println("N: Retrieves the employees name.");
 			System.out.println("S: Retrieves the employees salary.");
@@ -34,13 +45,21 @@ public class University_staff_roster {
 			System.out.println("Q: Returns to the main menu.");
 			action = input.next();
 			
-			if (action == "N") {
-				
+			if (action.equalsIgnoreCase("N")) {
+				System.out.println("The name of employee " + choice + " is: " + employee.getName());			}
+			if (action.equalsIgnoreCase("S")) {
+				System.out.println("The salary of employee " + choice + " is: " + employee.getSalary());			
+			}
+			if (action.equalsIgnoreCase("D")) {
+				System.out.println("The department of employee " + choice + " is: " + employee.getDepartment());			
+			}
+			if (action.equalsIgnoreCase("J")) {
+				System.out.println("The job title of employee " + choice + " is: " + employee.getJob());			
 			}
 		}
 		
-		
-		} while(choice != 0);
+		} while (choice != 0);
+		System.out.println("Shutting down application. Have a good day.");
 	}
 
 }
