334b486b664e8b0037958708c741b6f7cee81d52
38247004 <38247004@L815WD220024.edu.cbe.ab.ca>
2026-01-06 10:17:27 -0700

    Jan 6 2026

---
+++ Java_Project_Chapter_13/src/QueueTester.java
@@ -0,0 +1,22 @@
+
+public class QueueTester {
+
+	public static void main(String[] args) {
+		Queue q1 = new Queue(3);
+		
+		System.out.println("Adding red, yellow and orange");
+		
+		q1.enqueue("red");
+		q1.enqueue("yellow");
+		q1.enqueue("orange");
+		
+		System.out.println("Front of queue: " + q1.front());
+		System.out.println("Size of queue: " + q1.size());
+		
+		q1.dequeue();
+		
+		System.out.println("Front of queue: " + q1.front());
+		System.out.println("Size of queue: " + q1.size());
+	}
+
+}
